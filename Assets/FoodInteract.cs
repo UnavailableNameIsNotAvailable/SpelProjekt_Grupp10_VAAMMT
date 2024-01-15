c17893be737c4e0ccad1c7afd70c71ada6d3dc29
MercuriusLBS <156214121+MercuriusLBS@users.noreply.github.com>
2024-01-15 15:08:12 +0100

    Merge branch 'main' of https://github.com/UnavailableNameIsNotAvailable/SpelProjekt_Grupp10_VAAMMT

--- Assets/FoodInteract.cs
+++ Assets/FoodInteract.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class FoodInteract : MonoBehaviour
 {
+    HungerSystem hunger;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hunger = FindObjectOfType<HungerSystem>();
     }
 
     // Update is called once per frame
@@ -15,11 +19,17 @@ public class FoodInteract : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private async void OnCollisionEnter2D(Collision2D collision)
     {//Vid kollision så gömmer sig objektet.
         SpriteRenderer FoodRender = GetComponent<SpriteRenderer>();
         FoodRender.enabled = false;
         BoxCollider2D FoodCollider = GetComponent<BoxCollider2D>();
         FoodCollider.enabled = false;
+        hunger.AddHunger(500);
+        //Efter en slumpmässig antal sekunder så respawnar maten.
+        int waitingTime = Random.Range(40000,90000);
+        await Task.Delay(waitingTime);
+        FoodRender.enabled = true;
+        FoodCollider.enabled = true;
     }
 }
--- Assets/HungerSystem.cs
+++ Assets/HungerSystem.cs
@@ -1,21 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class HungerSystem : MonoBehaviour
 {
-    int HungerPoint;
+    public int HungerPoint;
     // Start is called before the first frame update
     void Start()
     {
-        HungerPoint = 100;
+        HungerPoint = 10000;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    async void FixedUpdate()
     {
-        HungerPoint -= 1;
-        Console.Write(HungerPoint);
+        HungerPoint -= 2;
+        await Task.Delay(10000);
+    }
+
+    public void AddHunger(int amount)
+    {
+        HungerPoint += amount;
     }
 }
