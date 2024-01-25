886d1f681fd836d9512d0c73915e89eb2aade1d6
UnavailableNameIsNotAvailable <154227444+UnavailableNameIsNotAvailable@users.noreply.github.com>
2024-01-25 12:42:00 +0100

    Update FoodInteract.cs

--- Assets/FoodInteract.cs
+++ Assets/FoodInteract.cs
@@ -30,7 +30,7 @@ public class FoodInteract : MonoBehaviour
         FoodRender.enabled = false;
         BoxCollider2D FoodCollider = GetComponent<BoxCollider2D>();
         FoodCollider.enabled = false;
-        hunger.AddHunger(8);
+        hunger.AddHunger(15);
         //Efter en slumpmässig antal sekunder mellan en och två minuter så respawnar maten.
         int waitingTime = Random.Range(60000,120000);
         await Task.Delay(waitingTime);
